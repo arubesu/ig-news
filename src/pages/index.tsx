21875712a3162a97079afb6133497df5fdd50d1c
Bruno Souza <bruno.arubesu@gmail.com>
2021-06-05 15:36:04 -0300

    feat(home page): add home page and style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,28 @@
-import { Header } from "../components/Header"
+import { Header } from "../components/Header";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <main className={styles.container}>
+        <section className={styles.contentContainer}>
+          <p>👏 Hey, Welcome </p>
+          <h1>News about <br />
+            the  <span>React</span> world
+          </h1>
+          <br />
+          <p>Get access to all the publications
+            <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img
+          src="/images/avatar.svg"
+          alt="girl coding"
+        />
+      </main>
+    </>
   )
 }
