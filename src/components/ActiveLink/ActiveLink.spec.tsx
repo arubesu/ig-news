76e12dd2ae7092eb04ab378bf6ba6bd72691aa28
Bruno Souza <bruno.arubesu@gmail.com>
2021-07-22 20:02:27 -0300

    test(activeLink):  testing whether receives active class

--- src/components/ActiveLink/ActiveLink.spec.tsx
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -11,11 +11,34 @@ jest.mock('next/router', () => {
   }
 })
 
-test('active link should render', () => {
-  const { getByText } = render(
-    <ActiveLink href="/" activeClassName="active">
-      <a>Test </a>
-    </ActiveLink>
-  )
-  expect(getByText('Test')).toBeInTheDocument()
+describe('Active Link Component', () => {
+
+  test('active link should render', () => {
+    const { getByText } = render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Test </a>
+      </ActiveLink>
+    )
+    expect(getByText('Test')).toBeInTheDocument()
+  })
+
+  test('active link should receive active class when href is equal to the current path', () => {
+    const { getByText } = render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Test </a>
+      </ActiveLink>
+    )
+    expect(getByText('Test')).toHaveClass('active')
+  })
+
+  test('active link should not receive active class when href is different to the current path', () => {
+    const { getByText } = render(
+      <ActiveLink href="/other" activeClassName="active">
+        <a>Test </a>
+      </ActiveLink>
+    )
+    expect(getByText('Test')).not.toHaveClass('active')
+  })
+
 })
+
