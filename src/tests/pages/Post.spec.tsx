bc27584e3e17d83fe115bf3c1292a675a323cc53
Bruno Souza <bruno.arubesu@gmail.com>
2021-07-26 17:00:17 -0300

    test(Post page): loads initial data when user has subscription

--- src/tests/pages/Post.spec.tsx
+++ src/tests/pages/Post.spec.tsx
@@ -49,4 +49,53 @@ describe('Posts page', () => {
       })
     )
   });
+
+  it('loads initial data when user has subscription', async () => {
+    const getSessionMocked = mocked(getSession)
+    const getPrismicClientMocked = mocked(getPrismicClient)
+
+    getSessionMocked.mockResolvedValueOnce({
+      activeSubscription: 'fake-subscription',
+    })
+
+    getPrismicClientMocked.mockResolvedValueOnce({
+      getByUID: jest.fn().mockResolvedValueOnce({
+        data: {
+          title: [{
+            type: 'heading',
+            text: 'post title'
+          }],
+          content: [{
+            type: 'paragraph',
+            text: 'post paragraph'
+          }],
+        },
+        last_publication_date: '2021-07-26'
+      })
+    })
+
+    const response = await getServerSideProps({
+      req: {
+        cookies: {},
+      },
+      params: {
+        slug: 'post-slug'
+      }
+    } as any)
+
+    const expectedPost = {
+      slug: 'post-slug',
+      title: 'post title',
+      content: '<p>post paragraph</p>',
+      updatedAt: 'July 25, 2021'
+    }
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          post: expectedPost,
+        }
+      })
+    )
+  });
 })
